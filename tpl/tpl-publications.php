c07fef70099249f0e892e65bdcb247415a7bcb09
Tim Smith <timsmith@ou.edu>
2019-11-19 10:24:53 -0600

    add links and contact form

---
+++ tpl/tpl-publications.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smit0015
+ * Date: 11/1/19
+ * Time: 10:23 AM
+ */ ?>
+
+
+<section id="publications">
+        <div class="container">
+            <div>
+                <h2>Explore our <br>Publications</h2>
+                <a href="https://giving.oufoundation.org/OnlineGivingWeb/Giving/OnlineGiving/Bizzell">Make a Gift</a>
+                <img src="https://ul-web-services.s3.amazonaws.com/img/vYf-cgzw.jpeg" />
+            </div>
+            <div>
+                <p>As the soul of the university, we are the hub of knowledge creation at OU. We connect students and faculty with the resources, technology, and skills that cross disciplines and unify the campuses. We help the university to attract top students, to recruit and retain outstanding faculty, to continue to build world-class research collections, and to attract external funding needed to advance OU research.</p>
+                <p>We invite you to invest in OU Libraries to give our scholars the tools to become champions and to forge the future fully prepared to take on the complex challenges we face as a society.</p>
+                <br>
+                <p>Sincerely,</p>
+                <br>
+                <br>
+                <p>
+                    <strong>Carl Grant</strong><br>
+Interim Dean of Libraries<br>
+                    The University of Oklahoma
+</p>
+            </div>
+        </div>
+    </section>
