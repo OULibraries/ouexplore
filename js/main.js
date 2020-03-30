dc6e06cc7dcfaef5b654758c0cdbb3e8a93ed259
Kyle Davis <kdaviswx@ou.edu>
2020-03-30 12:54:02 -0500

    rm old function for touch device

--- js/main.js
+++ js/main.js
@@ -438,21 +438,6 @@ function ontouch(el, callback) {
 	}, {passive: false})
 }
 
-// test if touch device
-function is_touch_device4() {
-	var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
-	var mq = function (query) {
-		return window.matchMedia(query).matches;
-	}
-	if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
-		return true;
-	}
-	// include the 'heartz' as a way to have a non matching MQ to help terminate
-	// the join https://git.io/vznFH
-	var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
-	return mq(query);
-}
-
 // open pillar by id passed in
 function openPillarPage (pillarID) {
 	$('.pillars, .pillarTitles').addClass('minimize');
