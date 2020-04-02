65c5c263b86ce1a362e9e1bfb01809bab9ebb16b
Kyle Davis <kdaviswx@ou.edu>
2020-04-02 14:42:41 -0500

    New mobile only contribute page. Had such different html, it needed to be seperate. Will load from main.js based on if on a phone or not.

---
+++ tpl/tpl-contribute_mobile.php
@@ -0,0 +1,23 @@
+
+    <section id="contribute">
+        <div class="container">
+            <div class="contrib_mobile">
+                <h1 style="display: none">A Page for making a contribution.</h1>
+                <h2>Make a <br>Contribution</h2>
+                <a href="https://giving.oufoundation.org/OnlineGivingWeb/Giving/OnlineGiving/Bizzell">Make a Gift</a>
+                <div class="mobile_contribute_div">
+                  <img class="mobile_carl_img" src="img/carl.jpg" alt="interim dean Carl Grant"/>
+                  <p>As the soul of the university, we are the hub of knowledge creation at OU. We connect students and faculty with the resources, technology, and skills that cross disciplines and unify the campuses. We help the university to attract top students, to recruit and retain outstanding faculty, to continue to build world-class research collections, and to attract external funding needed to advance OU research.</p>
+                  <p>We invite you to invest in OU Libraries to give our scholars the tools to become champions and to forge the future fully prepared to take on the complex challenges we face as a society.</p>
+                  <br>
+                  <p>Sincerely,</p>
+                  <br>
+                  <p>
+                    <strong>Carl Grant</strong><br>
+                    Interim Dean of Libraries<br>
+                    The University of Oklahoma
+                  </p>
+                </div>
+            </div>
+        </div>
+    </section>
